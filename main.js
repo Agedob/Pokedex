0f17a13f5fe7640ec9d70d60621e6d2062c32380
agedob <agedob888@gmail.com>
2018-01-31 21:17:11 -0800

    hour into it

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 function poke(){
     for(var i = 1; i < 152;++i){
-        $('div').append('<img id="' + i +'" src="http://pokeapi.co/media/img/' + i + '.png">')
+        $('.poke_pic').append('<img id="' + i +'" src="http://pokeapi.co/media/img/' + i + '.png">')
     };
 };
+function stats(img_id){
+    $.get("https://pokeapi.co/api/v2/pokemon/" + img_id + "/", function(res){
+        $('#name').append(res.name);
+        $('#list').append('<li>' + res.types[0].type.name + '</li>');
+        $('#Height').append(res.height);
+        $('#Weight').append(res.weight);
+        console.log(img_id);
+    });
+};
+
 
 $(document).ready(function(){
-    
+    poke();
+    $('img').click(function(complete){
+        var temp = ($(this).attr('id'));
+        stats(temp);
+    })
 })
