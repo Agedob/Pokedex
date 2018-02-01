e7e2895db41e43710be9333ae7d30b40c436e04c
agedob <agedob888@gmail.com>
2018-01-31 20:19:11 -0800

    inital commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function poke(){
+    for(var i = 1; i < 152;++i){
+        $('div').append('<img id="' + i +'" src="http://pokeapi.co/media/img/' + i + '.png">')
+    };
+};
+
+$(document).ready(function(){
+    
+})
