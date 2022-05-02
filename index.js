5a193ebc7486c92ff913946b717ef5aeecc20b4b
MЯ.ƧᄂΣΣPY <agedob888@gmail.com>
2022-05-02 13:57:27 -0700

    image click id working

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 
 $(document).ready(function(){
-    for( var i = 1; i < 152; i++){
-
+    for( var i = 1; i < 11; i++){
     $('#container').append('<img id="' + i + '" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + i + '.png">' )
-
     }
 
+    $(document).on('click', 'img', function(){
+        // console.log($(this).attr('id'))
+    })
+
 })
+
+// $.get("https://pokeapi.co/api/v2/pokemon/1/", function(res) {
+//     var html_str = "";
+//     html_str += "<h4>Types</h4>";
+//     html_str += "<ul>"; 
+//     for(var i = 0; i < res.types.length; i++) {
+//         html_str += "<li>" + res.types[i].type.name + "</li>";
+//     }
+//     html_str += "</ul>";
+//     $("#bulbasaur").html(html_str);
+// }, "json");
