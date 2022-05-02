73346b92c64197bb0d0fa2ecdb421e8e4a32136e
MЯ.ƧᄂΣΣPY <agedob888@gmail.com>
2022-05-02 13:52:08 -0700

    new project

---
+++ index.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    for( var i = 1; i < 152; i++){
+
+    $('#container').append('<img id="' + i + '" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + i + '.png">' )
+
+    }
+
+})
