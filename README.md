55d0858e4e3e02a0d281b9b8acabe23d85f37d9e
MЯ.ƧᄂΣΣPY <agedob888@gmail.com>
2022-05-02 15:23:07 -0700

    done

---
+++ README.md
@@ -0,0 +1,13 @@
+# Pokedex Jquery & json 
+
+1) images of 1-151
+2) clickable for info
+    * type
+    * height
+    * Weight
+
+# Improvements 
+
+1) Name 
+2) pull photo to pokedex
+3) .trigger()
--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 
 $(document).ready(function(){
-    for( var i = 1; i < 11; i++){
-    $('#container').append('<img id="' + i + '" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + i + '.png">' )
+
+    for( var i = 1; i < 152; i++){
+    $('#pokemon').append('<img id="' + i + '" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + i + '.png">' )
     }
 
     $(document).on('click', 'img', function(){
         // console.log($(this).attr('id'))
-    })
+        var id = $(this).attr('id')
+        $.get("https://pokeapi.co/api/v2/pokemon/" + id + "/", function(res) {
+            var html_str = "";
+            html_str += "<h4>Name : " + res.name.toUpperCase() + "</h4>";
+            html_str += '<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + id + '.png">'
+            html_str += "<ul> Type(s):"; 
+            for(var i = 0; i < res.types.length; i++) {
+                html_str += "<li>" + res.types[i].type.name + "</li>";
+            }
+            html_str += "</ul>";
+            html_str += "<p> Height: " + res.height + "</p>"
+            html_str += "<p> Weight: " + res.weight + 'lb</p>'
+            $("#pokedex_container").html(html_str);
+            console.log(res)
+            }, "json");
+        })
+
+    $('#1').trigger('click')
 
 })
 
-// $.get("https://pokeapi.co/api/v2/pokemon/1/", function(res) {
-//     var html_str = "";
-//     html_str += "<h4>Types</h4>";
-//     html_str += "<ul>"; 
-//     for(var i = 0; i < res.types.length; i++) {
-//         html_str += "<li>" + res.types[i].type.name + "</li>";
-//     }
-//     html_str += "</ul>";
-//     $("#bulbasaur").html(html_str);
-// }, "json");
